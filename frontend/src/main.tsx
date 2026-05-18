2ae0d1f59cc75ed8b7b1e3b83f42a7f229e80ee6
vedantnaik09 <vedant.naik09@gmail.com>
2026-05-18 10:17:55 +0530

    refactor: replace GeneratorPage with App component in main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import GeneratorPage from './pages/Generator'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GeneratorPage />
+    <App />
   </StrictMode>,
 )
